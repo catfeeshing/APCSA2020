b1533cda1713b7ef816b43f70927d4e6caea65b5
cuic0644 <cuic0644@CA-SU-F106-22.sduhsd.lan>
2020-01-31 10:25:44 -0800

    1/31/20 - Upload Unit0 - school

---
+++ APCS-A-2020/src/Unit0/Variables.java
@@ -0,0 +1,56 @@
+package Unit0;
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class Variables
+{
+	public static void main ( String[] args )
+	{
+		//define 1 variable of each of the
+		//following data types
+		//byte <		short <		int < 		long
+		//float		double
+		//char      boolean		String
+
+		//integer variables
+		byte byteOne = 127;
+		short shortOne = 2;
+		int intOne = 
+
+		//decimal variables
+		float floatOne = 12343849283483048039;
+		double doubleOne = Math.PI;
+
+
+		//other integer types
+		char charOne = A;
+
+		//other types
+		boolean boolOne = true;
+
+
+		//output your information here
+		System.out.println("/////////////////////////////////");
+		System.out.println("*                               *");
+		System.out.println("*        integer types          *");
+		System.out.println("*                               *");
+		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
