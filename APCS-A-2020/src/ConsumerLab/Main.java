aa8859030876eb49ca640a5b292f59339e9430ae
catfeeshing <33041211+catfeeshing@users.noreply.github.com>
2020-02-19 22:51:30 -0800

    2-19-20 - add print statements - home

--- APCS-A-2020/src/ConsumerLab/Main.java
+++ APCS-A-2020/src/ConsumerLab/Main.java
@@ -6,10 +6,16 @@ import java.util.Scanner;
 class Main {
 	public static void main(String[] args) {
 
-		System.out.println(Review.totalSentiment("src/ConsumerLab/cleanSentiment.csv"));
+		//System.out.println(Review.totalSentiment("src/ConsumerLab/cleanSentiment.csv"));
 		
-		// test reading from files
+		//printy print
+		System.out.println(Review.totalSentiment("src/ConsumerLab/simpleReview.txt"));
+		System.out.println(Review.starRating("src/ConsumerLab/simpleReview.txt"));
+		System.out.println(Review.fakeReview("src/ConsumerLab/simpleReview.txt"));
+		System.out.println(Review.positiveReview("src/ConsumerLab/simpleReview.txt"));
+		System.out.println(Review.negativeReview("src/ConsumerLab/simpleReview.txt"));
 		// read in the positive adjectives in postiveAdjectives.txt
+		//? is this the intention System.out.println(Review.totalSentiment("src/ConsumerLab/positiveAdjectives.txt"));
 		try {
 			Scanner input = new Scanner(new File("src/ConsumerLab/positiveAdjectives.txt"));
 			while (input.hasNextLine()) {
